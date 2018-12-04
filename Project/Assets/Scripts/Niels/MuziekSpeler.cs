983a22b2c7596998e3ad2cfbadd44fa2ceefc5df
niels <niels-duivenvoorden@hotmail.com>
2018-12-04 12:41:29 +0100

    settings

--- Project/Assets/Scripts/Niels/MuziekSpeler.cs
+++ Project/Assets/Scripts/Niels/MuziekSpeler.cs
@@ -2,25 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MuziekSpeler : MonoBehaviour {
+public class MuziekSpeler : MonoBehaviour
+{
+	public float musicVolume = 1f;
 
-	static MuziekSpeler instance = null;
+    static MuziekSpeler instance = null;
 
-	void Start (){
-		if (instance != null){
-			Destroy (gameObject);
-		}else{	
-			instance = this;
-			DontDestroyOnLoad(gameObject);//als er al een nummer afspeelt wordt het niet verwijderd
-		}
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);//als er al een nummer afspeelt wordt het niet verwijderd
+        }
 
-	}
-	void Awake(){
-		string mute = PlayerPrefs.GetString ("Mute");
-		if (mute == "true") {
-			AudioListener.pause = true;//geluid uit
-		} else {
-			AudioListener.pause = false;//geluid aan
-		}
-	}
+    }
+
+    void update()
+    {
+		AudioListener.volume = musicVolume;
+    }
+
+    void Awake()
+    {
+        string mute = PlayerPrefs.GetString("Mute");
+        if (mute == "true")
+        {
+            AudioListener.pause = true;//geluid uit
+        }
+        else
+        {
+            AudioListener.pause = false;//geluid aan
+        }
+    }
+
+    public void SetVolume(float vol)
+    {
+        musicVolume = vol;
+    }
 }
--- Project/Assets/Scripts/Niels/Volume.cs
+++ Project/Assets/Scripts/Niels/Volume.cs
@@ -1,15 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿﻿using UnityEngine;
 
-public class Volume : MonoBehaviour
-{
-    public Slider volumeSlider;
-    public AudioSource volumeAudio;
+public class Volume : MonoBehaviour {
+
+    // Reference to Audio Source component
+    private AudioSource audioSrc;
+
+    // Music volume variable that will be modified
+    // by dragging slider knob
+    private float musicVolume = 1f;
+
+	// Use this for initialization
+	void Start () {
+
+        // Assign Audio Source component to control it
+        audioSrc = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Setting volume option of Audio Source to be equal to musicVolume
+        
+	}
+
+    // Method that is called by slider game object
+    // This method takes vol value passed by slider
+    // and sets it as musicValue
     
-    public void VolumeController()
-    {
-        volumeAudio.volume =volumeSlider.value;
-    }
 }
