b74e0b273b71d27cc1127e439ebb90c7b82e3733
niels <niels-duivenvoorden@hotmail.com>
2018-12-07 10:42:17 +0100

    screens

---
+++ Project/Assets/Scripts/Niels/Screenshot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Generate a screenshot and save to disk with the name SomeLevel.png.
+
+public class Screenshot : MonoBehaviour
+{
+    public void Screen()
+    {
+        Application.CaptureScreenshot("eric", );
+    }
+}
