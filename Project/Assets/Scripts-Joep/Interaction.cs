0075aba10cae6ee2a479a305b9fe9630b45ed992
PukingUnicornZ <36401329+PukingUnicornZ@users.noreply.github.com>
2018-12-07 10:19:55 +0100

    FUCKYALL
    made my shit

---
+++ Project/Assets/Scripts-Joep/Interaction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction : MonoBehaviour {
+    public Transform player;
+    public Text UItext;
+    public float range;
+    public string Message;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(player.position, transform.position) <= range)
+        {
+            UItext.text = "" + Message;
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                DOTHIS();
+            }
+        }
+        else
+        {
+            UItext.text = "";
+        }
+	}
+    public void DOTHIS()
+    {
+        print("DOTHISCUNT");
+    }
+}
