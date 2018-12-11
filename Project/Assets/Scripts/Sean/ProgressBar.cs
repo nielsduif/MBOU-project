ba8b2d5d299be17c8ff8a4a7e5f7bd9a52fed731
niels <niels-duivenvoorden@hotmail.com>
2018-12-11 13:26:54 +0100

    level design + test scene

--- Project/Assets/Scripts/Sean/ProgressBar.cs
+++ Project/Assets/Scripts/Sean/ProgressBar.cs
@@ -3,33 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ProgressBar : MonoBehaviour {
+public class ProgressBar : MonoBehaviour
+{
 
-    public float CurrentQuestions { get; set; }
-    public float MaxQuestions { get; set; }
+    public float CurrentQuestions; // { get; set; }
+    public float MaxQuestions; // { get; set; }
     public Slider Progress;
 
     // Verander maxquestions met het aantal vragen die we hebben
-    void Start () {
-        MaxQuestions = 3f;
+    void Start()
+    {
+        if (MaxQuestions == 0)
+        {
+            MaxQuestions = 3f;
+        }
+
         CurrentQuestions = 0f;
 
-        Progress.value = CurrentQuestions;
-	}
-	
-	// We moeten na elke vraag die we goed hebben bijv vraagGoed() returnen, zodat we If(vraagGoed) kunnen doen ipv getkeydown
-	void Update () {
+        //Progress.value = CurrentQuestions;
+    }
+
+    // We moeten na elke vraag die we goed hebben bijv vraagGoed() returnen, zodat we If(vraagGoed) kunnen doen ipv getkeydown
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.X))
         {
             CurrentQuestions += 1;
         }
         Progress.value = CurrentQuestions;
-	}
+    }
 
-    public void goedAntwoord ()
+    public void goedAntwoord()
     {
         CurrentQuestions += 1;
     }
 
-    
+
 }
