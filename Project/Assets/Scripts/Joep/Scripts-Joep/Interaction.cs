ad7ad5b77c93c4fca0c540a5136bf6c4c96c9e2d
niels <niels-duivenvoorden@hotmail.com>
2019-01-11 10:33:18 +0100

    fix

--- Project/Assets/Scripts/Joep/Scripts-Joep/Interaction.cs
+++ Project/Assets/Scripts/Joep/Scripts-Joep/Interaction.cs
@@ -3,40 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Interaction : MonoBehaviour {
+public class Interaction : MonoBehaviour
+{
 
     public Transform player;
     public Text UItext;
     public float range;
     public string Message;
     public GameObject SchermCanvas;
-    public GameObject UI;
+    public string scherm;
+    //public GameObject UI;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+        UItext.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Vector3.Distance(player.position, transform.position) <= range)
         {
             UItext.text = "" + Message;
             if (Input.GetKeyDown(KeyCode.E))
             {
                 DOTHIS();
+                UItext.text = "";
             }
         }
         else
         {
             UItext.text = "";
         }
-	}
+    }
     public void DOTHIS()
     {
         //print("DOTHISCUNT");
-        UI.SetActive(false);
+        //UI.SetActive(false);
+        PlayerPrefs.SetString("onderwijsscherm", scherm);
+        PlayerPrefs.SetInt("menu", 1);
+        PlayerPrefs.SetInt("sensx", 0);
+        PlayerPrefs.SetInt("sensy", 0);
         SchermCanvas.SetActive(true);
     }
 }
